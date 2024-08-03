26411392c8385be8ba9cb1de5e61f1113cbcf364
Asaf Shakarzy <asaf@crossid.io>
2024-08-03 16:11:50 +0300

    chore: Dockerfile
    resolves #12

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: '/portal',
+  basePath: '/portal',
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
